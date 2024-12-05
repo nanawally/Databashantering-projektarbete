c6d5badbabb80aaeda57e53a7c2dd58521cef1de
nanawally <anoli89@hotmail.com>
2024-12-05 19:06:32 +0100

    commit

--- src/main/java/se/anna/employeedatabase/Employee.java
+++ src/main/java/se/anna/employeedatabase/Employee.java
@@ -1,4 +1,53 @@
 package se.anna.employeedatabase;
 
 public class Employee {
+    private Integer employeeId;
+    private String name;
+    private String email;
+    private String password;
+    private WorkRole roleid;
+
+    public Employee(Integer employeeId, String name, String email, String password, WorkRole roleid) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roleid = roleid;
+    }
+
+    public Employee(String name, String email, String password, WorkRole roleid) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roleid = roleid;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public WorkRole getRoleid() {
+        return roleid;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee ID: " + employeeId +
+                ", Name: " + name +
+                ", Email: " + email +
+                ", Password: " + password +
+                ", Role ID: " + roleid;
+    }
 }
--- src/main/java/se/anna/employeedatabase/WorkRole.java
+++ src/main/java/se/anna/employeedatabase/WorkRole.java
@@ -1,4 +1,55 @@
 package se.anna.employeedatabase;
 
+import java.util.*;
+
 public class WorkRole {
+    private Integer roleId;
+    private String title;
+    private String description;
+    private double salary;
+    private Date creationDate;
+
+    public WorkRole(Integer roleId, String title, String description, double salary, Date creationDate) {
+        this.roleId = roleId;
+        this.title = title;
+        this.description = description;
+        this.salary = salary;
+        this.creationDate = creationDate;
+    }
+
+    public WorkRole(String title, String description, double salary, Date creationDate) {
+        this.title = title;
+        this.description = description;
+        this.salary = salary;
+        this.creationDate = creationDate;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Role ID: " + roleId +
+                ", Title: " + title +
+                ", Description: " + description +
+                ", Salary: " + salary +
+                ", Creation Date: " + creationDate;
+    }
 }
